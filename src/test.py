a200054ce30e36c2e39a0848aa17bf68f7fa9699
HUASHENG <331292108@qq.com>
2021-01-27 22:30:22 +0800

    调整AGNews数据集的输入格式

--- src/test.py
+++ src/test.py
@@ -2,15 +2,17 @@ import pandas as pd
 from tqdm import tqdm
 import torch
 
-# f = pd.read_csv('../data/SICK/SICK.txt', sep='\t', quoting=3)
+# f = pd.read_csv('../data/AGNews/test.csv', sep='\t', quoting=3)
+f = pd.read_csv('../data/AGNews/train.csv')
 
-# for index, row in tqdm(f.iterrows()):
+for index, row in tqdm(f.iterrows()):
+    print(row)
     # print(row['#1 String'])
     # print(row['entailment_label'])
 
-a = torch.nn.Parameter(torch.FloatTensor(2), requires_grad=False)
-
-a.data.fill_(0.1)
-
-for i in range(1, 5):
-    print(i)
+# a = torch.nn.Parameter(torch.FloatTensor(2), requires_grad=False)
+#
+# a.data.fill_(0.1)
+#
+# for i in range(1, 5):
+#     print(i)
